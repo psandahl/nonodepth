8a9c36c0d7e4cffadf2056fb4afb4e27429d9fca
Patrik Sandahl <patrik.sandahl@gmail.com>
2023-03-10 22:37:56 +0100

    SSIM.

--- nonodepth/__main__.py
+++ nonodepth/__main__.py
@@ -5,7 +5,7 @@ from torchvision.transforms import Resize, InterpolationMode
 import matplotlib.pyplot as plt
 
 from nonodepth.datasets.diode import Diode
-from nonodepth.utils.image import tensor_to_np_image
+from nonodepth.utils.image import tensor_to_np_image, SSIM
 
 
 def view_triplet(sample: tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> None:
@@ -27,6 +27,33 @@ def view_triplet(sample: tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> Non
     plt.show()
 
 
+def view_ssim(x: torch.Tensor, y: torch.Tensor) -> None:
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(tensor_to_np_image(x), cmap='gray',
+               vmin=torch.min(x), vmax=torch.max(x))
+    plt.title('Image X')
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(tensor_to_np_image(y), cmap='gray',
+               vmin=torch.min(y), vmax=torch.max(y))
+    plt.title('Image Y')
+
+    ssim = SSIM()
+    ssim_img = ssim(x, y)
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(tensor_to_np_image(ssim_img), cmap='gray',
+               vmin=torch.min(ssim_img), vmax=torch.max(ssim_img))
+    plt.title('SSIM')
+
+    plt.show()
+
+    ssim_score = ssim.score(x, y)
+
+    print(f'ssim_score={ssim_score.item()}')
+
+
 if __name__ == '__main__':
     path = pathlib.Path('C:\\Users\\patri\\datasets\\val')
 
@@ -39,4 +66,6 @@ if __name__ == '__main__':
 
     print(f'Available samples={len(diode)}')
 
-    view_triplet(diode[44])
+    triplet = diode[44]
+    view_triplet(triplet)
+    view_ssim(triplet[1], triplet[1])
--- nonodepth/utils/image.py
+++ nonodepth/utils/image.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import numpy as np
 import torch
+import torch.nn as nn
 
 
 def tensor_to_np_image(img: torch.Tensor) -> np.ndarray:
@@ -33,3 +36,48 @@ def np_to_tensor_image(img: np.ndarray) -> torch.Tensor:
         return torch.from_numpy(img)
     else:
         return None
+
+
+class SSIM(nn.Module):
+    """
+    Layer to compute the SSIM image from a pair of images. Inspired from: Monodepth2.
+    """
+
+    def __init__(self: SSIM) -> None:
+        super(SSIM, self).__init__()
+        self.mu_x_pool = nn.AvgPool2d(3, 1)
+        self.mu_y_pool = nn.AvgPool2d(3, 1)
+        self.sig_x_pool = nn.AvgPool2d(3, 1)
+        self.sig_y_pool = nn.AvgPool2d(3, 1)
+        self.sig_xy_pool = nn.AvgPool2d(3, 1)
+
+        self.refl = nn.ReflectionPad2d(1)
+
+        self.C1 = 0.01 ** 2
+        self.C2 = 0.03 ** 2
+
+    def forward(self: SSIM, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        x = self.refl(x)
+        y = self.refl(y)
+
+        mu_x = self.mu_x_pool(x)
+        mu_y = self.mu_y_pool(y)
+
+        sigma_x = self.sig_x_pool(x ** 2) - mu_x ** 2
+        sigma_y = self.sig_y_pool(y ** 2) - mu_y ** 2
+        sigma_xy = self.sig_xy_pool(x * y) - mu_x * mu_y
+
+        SSIM_n = (2 * mu_x * mu_y + self.C1) * (2 * sigma_xy + self.C2)
+        SSIM_d = (mu_x ** 2 + mu_y ** 2 + self.C1) * \
+            (sigma_x + sigma_y + self.C2)
+
+        # return torch.clamp((1 - SSIM_n / SSIM_d) / 2, 0, 1)
+        return SSIM_n / SSIM_d
+
+    def score(self: SSIM, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        """
+        Compute an inverse SSIM score, where zero is perfect match and one
+        is negative match.
+        """
+        ssim_img = self.forward(x, y)
+        return torch.clamp((1. - ssim_img.mean()) / 2., 0., 1.0)
